efebe777a1ab22a540f2bc553c70a035e0928287
ryuji-saitou <rs0623idmanagement@gmail.com>
2021-07-01 09:28:12 +0000

    [finish] chapter02

--- chapter02/app/controllers/books_controller.rb
+++ chapter02/app/controllers/books_controller.rb
@@ -1,11 +1,16 @@
 class BooksController < ApplicationController
 
+  before_action :ensure_correct_user, only: [:edit, :update]
+
   def show
+    @book_new = Book.new
     @book = Book.find(params[:id])
+    @user = User.find(@book.user_id)
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
@@ -23,8 +28,6 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
-
-
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
@@ -34,16 +37,24 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
+  end
+
+ def ensure_correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    unless @user == current_user
+      redirect_to books_path
+    end
   end
 
 end
